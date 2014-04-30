4e17e15cfad03a03b2a4a157c484b0605a3db060
Hendrixer <willscottmoss@gmail.com>
2014-04-30 16:09:40 -0700

    (bug) debug travis

--- src/animationClass.js
+++ src/animationClass.js
@@ -150,7 +150,7 @@
         options.stagger = true;
         Assist.addTimer(options, element, done);
         var leave = new TimelineMax();
-        console.log('leave here', leave);
+        console.log( 'element: ', element, '====end',end);
         leave.to(element, end);
         leave.to(element, options.duration, third);
         leave.to(element, options.duration, mid);
