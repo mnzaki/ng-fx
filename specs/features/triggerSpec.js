a34cc02694b746c11e307a634076beab2158ed05
Hendrixer <willscottmoss@gmail.com>
2014-07-12 00:32:54 -0700

    bug(spec) update specs fot version 1.0, add nyan and unicorn reporter

--- specs/features/triggerSpec.js
+++ specs/features/triggerSpec.js
@@ -2,23 +2,15 @@ describe('Trigger feature', function(){
   var scope,
       element;
   beforeEach(module('fx.animations.assist'));
-  beforeEach(module(function($controllerProvider){
-    $controllerProvider.register('testController', function($scope){
-      $scope.name = 'test';
-    });
-  }));
 
   it('should return a new animation instance', function(){
-    inject(function($rootScope, $controller, $compile, Assist){
-      scope = $rootScope.$new();
-      $controller('testController', {
-        $scope: scope
-      });
-      element = $compile('<div ng-controller="testController"class="fx-trigger">trigger</div>')(scope);
+    inject(function($rootScope, $compile, Assist){
+
+      element = $compile('<div class="fx-trigger">trigger</div>')($rootScope);
       $rootScope.$digest();
-       spyOn(scope, '$emit');
+       spyOn($rootScope, '$broadcast');
       Assist.emit(element, 'fade-normal', 'enter');
-      check(scope.$emit).toHaveBeenCalledWith('fade-normal enter');
+      check($rootScope.$broadcast).toHaveBeenCalledWith('fade-normal:enter');
     });
   });
 });
