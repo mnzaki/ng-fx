33dcf69bd46b32ddc444742a6405b124e756eff1
Hendrixer <willscottmoss@gmail.com>
2015-07-25 14:03:56 -0500

    chore(refactor) prep for v2

--- src/animations/fxAnimations.js
+++
@@ -1,8 +0,0 @@
-angular.module('fx.animations', [
-  'fx.animations.fades',
-  'fx.animations.bounces',
-  'fx.animations.zooms',
-  'fx.animations.rotations',
-  'fx.animations.tada',
-  'fx.animations.pulse'
-]);
