b101d2c2e5bc353c4239714d78c9c0ea090ab75b
Hendrixer <willscottmoss@gmail.com>
2015-11-02 19:13:37 -0800

    fix(specs): add pllyfill for function bind for phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      './node_modules/phantomjs-polyfill/bind-polyfill.js',
         { pattern: 'spec.bundle.js', watched: false }
     ],
 
